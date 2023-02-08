9da1624dc924b8ef4793d47b27c76242ec4d258f
Bars0_o <maksterekhov1704@gmail.com>
2023-02-08 17:42:57 +0200

    simple wisualization

--- Assets/Editor/MarkovSimEditor.cs
+++ Assets/Editor/MarkovSimEditor.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using MarkovTest;
 using MarkovTest.Serialization;
 using MarkovTest.TwoDimension;
+using MarkovTest.TwoDimension.Patterns;
+using MarkovTest.TwoDimension.Rules;
+using MarkovTest.TwoDimension.Sequences;
 using UnityEditor;
 using UnityEngine;
 
@@ -46,6 +50,183 @@ public class MarkovSimEditor : Editor
             sim.Simulation = SimulationFabric.Labirynth();
             Save();
         }
+
+        DrawSim(sim.Simulation, sim);
+    }
+
+
+    private static void DrawSim(MarkovSimulationTwoDim<byte> sim2dim, MarkovSimulation sim)
+    {
+        if (sim2dim.Seed != null)
+            EditorGUILayout.IntField("Seed", sim2dim.Seed.Value);
+        else
+            GUILayout.Label("Random Seed");
+        
+        EditorGUILayout.Vector2Field("Size", new Vector2(sim2dim.Size.X, sim2dim.Size.Y));
+        DrawPlayablesList(sim2dim.Playables, sim);
+    }
+
+    private static void DrawPattern(Pattern<Byte> pattern, MarkovSimulation sim)
+    {
+        GUILayout.BeginHorizontal();
+
+        GUILayout.BeginVertical();
+        for (var x = 0; x < pattern.PatternForm.GetLength(0); x++)
+        {
+            GUILayout.BeginHorizontal();
+            for (var y = 0; y < pattern.PatternForm.GetLength(1); y++)
+            {
+                DrawPatternElement(pattern.PatternForm[x, y], sim);
+            }
+
+            GUILayout.EndHorizontal();
+        }
+
+        GUILayout.EndVertical();
+
+        GUILayout.EndHorizontal();
+        GUILayout.Space(100);
+    }
+
+
+    private static void DrawRotationSettings(RotationSettingsFlags settings)
+    {
+        GUILayout.BeginVertical();
+        GUILayout.Toggle(settings.HasFlag(RotationSettingsFlags.Rotate), "Rotate");
+        GUILayout.Toggle(settings.HasFlag(RotationSettingsFlags.FlipX), "FlipX");
+        GUILayout.Toggle(settings.HasFlag(RotationSettingsFlags.FlipY), "FlipY");
+        GUILayout.EndVertical();
+    }
+
+    private static void DrawStamp(byte[,] stamp, MarkovSimulation sim)
+    {
+        GUILayout.BeginVertical();
+        for (var x = 0; x < stamp.GetLength(0); x++)
+        {
+            GUILayout.BeginHorizontal();
+            for (var y = 0; y < stamp.GetLength(1); y++)
+            {
+                DrawStampElement(stamp[x, y], sim);
+            }
+
+            GUILayout.EndHorizontal();
+        }
+
+        GUILayout.EndVertical();
+
+        GUILayout.Space(100);
+    }
+
+
+    private static void DrawSequence(SequenceBase<byte> sequence, MarkovSimulation sim)
+    {
+        if (sequence is CycleSequence<byte> cycle)
+            DrawCycleSequence(cycle, sim);
+        else if (sequence is MarkovSequence<byte> markov)
+            DrawMarkovSequence(markov, sim);
+    }
+
+    private static void DrawCycleSequence(CycleSequence<byte> cycleSequence, MarkovSimulation sim)
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Cycle");
+        EditorGUILayout.FloatField(cycleSequence.Cycles);
+        GUILayout.EndHorizontal();
+        DrawPlayablesList(cycleSequence.Playables, sim);
+    }
+
+    private static void DrawMarkovSequence(MarkovSequence<byte> markovSequence, MarkovSimulation sim)
+    {
+        GUILayout.Label("Markov");
+        DrawPlayablesList(markovSequence.Playables, sim);
+    }
+
+
+    private static void DrawPlayablesList(List<ISequencePlayable<byte>> playables, MarkovSimulation sim)
+    {
+        GUILayout.BeginVertical();
+        playables.ForEach(x =>
+        {
+            DrawPlayable(x, sim);
+            GUILayout.Space(10);
+        });
+        GUILayout.EndVertical();
+    }
+
+    private static void DrawPlayable(ISequencePlayable<byte> playable, MarkovSimulation sim)
+    {
+        if (playable is RuleBase<byte> rule)
+            DrawRule(rule, sim);
+        else if (playable is SequenceBase<byte> sequence)
+            DrawSequence(sequence, sim);
+    }
+
+
+    private static void DrawRule(RuleBase<byte> rule, MarkovSimulation sim)
+    {
+        if (rule is RandomRule<byte> randRule)
+            DrawRandomRule(randRule, sim);
+        else if (rule is AllRule<byte> allRule)
+            DrawAllRule(allRule, sim);
+    }
+
+    private static void DrawAllRule(AllRule<byte> rule, MarkovSimulation sim)
+    {
+        GUILayout.Label("All");
+        GUILayout.BeginHorizontal();
+
+        DrawPattern(rule.MainPattern, sim);
+        DrawRotationSettings(rule.RotationSettings);
+        GUILayout.Label("->");
+        DrawStamp(rule.Stamp, sim);
+
+        GUILayout.EndHorizontal();
+    }
+
+    private static void DrawRandomRule(RandomRule<byte> rule, MarkovSimulation sim)
+    {
+        GUILayout.Label("Random");
+        GUILayout.BeginHorizontal();
+
+        DrawPattern(rule.MainPattern, sim);
+        DrawRotationSettings(rule.RotationSettings);
+        GUILayout.Label("->");
+        DrawStamp(rule.Stamp, sim);
+
+        GUILayout.EndHorizontal();
+    }
+
+    private static void DrawStampElement(byte stampElement, MarkovSimulation sim)
+    {
+        var style = new GUIStyle();
+        style.normal.background = Texture2D.whiteTexture;
+        style.margin = new RectOffset(5, 5, 0, 0);
+
+        var defaultColor = GUI.backgroundColor;
+
+
+        GUI.backgroundColor = sim.ColorPaletteLink.GetColor(stampElement);
+        GUILayout.Button("", style, GUILayout.Width(20), GUILayout.Height(20));
+        GUI.backgroundColor = defaultColor;
+    }
+
+    private static void DrawPatternElement(IEquatable<byte> patternElement, MarkovSimulation sim)
+    {
+        var style = new GUIStyle();
+        style.normal.background = Texture2D.whiteTexture;
+        style.margin = new RectOffset(5, 5, 0, 0);
+
+        var defaultColor = GUI.backgroundColor;
+
+        if (!(patternElement is byte))
+        {
+            Debug.LogError($"{patternElement} doesnt has drawer!");
+            return;
+        }
+
+        GUI.backgroundColor = sim.ColorPaletteLink.GetColor(((byte)patternElement));
+        GUILayout.Button("", style, GUILayout.Width(20), GUILayout.Height(20));
+        GUI.backgroundColor = defaultColor;
     }
 
     private static void DrawPalette(MarkovSimulation sim)
@@ -97,15 +278,15 @@ public class MarkovSimEditor : Editor
         if (!_sceneContext.IsActive())
             _sceneContext.Enter();
         MarkovSimulation sim = (MarkovSimulation)target;
-        
+
         sim.Visualize(_sceneContext.rootGameObject);
 
         void OnSimulationOnOnSimulationChanged(byte[,] bytes) => sim.Visualize(_sceneContext.rootGameObject);
 
         sim.Simulation.OnSimulationChanged += OnSimulationOnOnSimulationChanged;
-        
+
         sim.Simulation.Play(sim.Simulation);
-        
+
         sim.Simulation.OnSimulationChanged -= OnSimulationOnOnSimulationChanged;
     }
 }
